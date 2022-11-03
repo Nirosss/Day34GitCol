9e763f9a7c7cf8877bb0e98aedb23174e49d9bda
Nir Roiter <aniroiter@gmail.com>
2022-11-03 17:34:57 +0900

    added infowindow inputk

--- js/app.controller.js
+++ js/app.controller.js
@@ -10,106 +10,111 @@ window.onChooseLoc = onChooseLoc
 window.onDeleteLoc = onDeleteLoc
 window.onCopyLoc = onCopyLoc
 window.onAddressSearch = onAddressSearch
+window.onSetLocationName = onSetLocationName
 
 function onInit() {
-    // const params = new Proxy(new URLSearchParams(window.location.search), {
-    //     get: (searchParams, prop) => searchParams.get(prop),
-    // })
-    // const lat = params.lat
-    // const lng = params.lng
-    mapService
-        .initMap()
-        .then(() => {
-            onGetLocs()
-        })
-        .catch(() => console.log('Error: cannot init map'))
+  // const params = new Proxy(new URLSearchParams(window.location.search), {
+  //     get: (searchParams, prop) => searchParams.get(prop),
+  // })
+  // const lat = params.lat
+  // const lng = params.lng
+  mapService
+    .initMap()
+    .then(() => {
+      onGetLocs()
+    })
+    .catch(() => console.log('Error: cannot init map'))
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-    console.log('Getting Pos')
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
+  console.log('Getting Pos')
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
 }
 
 function onAddMarker() {
-    console.log('Adding a marker')
-    mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 }, 'Hello World')
+  console.log('Adding a marker')
+  mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 }, 'Hello World')
 }
 
 function onGetLocs() {
-    locService.getLocs().then((locs) => {
-        console.log('Locations:', locs)
-        // document.querySelector('.locs').innerText = JSON.stringify(
-        //     locs,
-        //     null,
-        //     2
-        // )
-        // let locsHTML = '<ul>'
-        let locsHTML ='<table class="rwd-table">'
-        locs.map((loc) => {
-            const { id, lat, lng, name } = loc
-            locsHTML += `
+  locService.getLocs().then((locs) => {
+    // document.querySelector('.locs').innerText = JSON.stringify(
+    //     locs,
+    //     null,
+    //     2
+    // )
+    // let locsHTML = '<ul>'
+    let locsHTML = '<table class="rwd-table">'
+    locs.map((loc) => {
+      const { id, lat, lng, name } = loc
+      locsHTML += `
             <tr>
                 <td class="place-name">${name}</td>
                 <td><button class="table-btn" onclick="onChooseLoc(${lat}, ${lng})">GoTo</button></td>
                 <td><button class="table-btn" onclick="onCopyLoc(${lat}, ${lng})")>Copy Link</button></td>
                 <td><button class="table-btn" onclick="onDeleteLoc(${id})">Delete</button></td>
             </tr>`
-            mapService.addMarker({ lat, lng }, name)
-        })
-        locsHTML += '</table>'
-        document.querySelector('.container').innerHTML = locsHTML
+      mapService.addMarker({ lat, lng }, name)
     })
+    locsHTML += '</table>'
+    document.querySelector('.container').innerHTML = locsHTML
+  })
 }
 
 function onGetUserPos() {
-    getPosition()
-        .then((pos) => {
-            console.log('User position is:', pos.coords)
-            document.querySelector(
-                '.user-pos'
-            ).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
-            mapService.panTo(pos.coords.latitude, pos.coords.longitude)
-            mapService.addMarker(
-                {
-                    lat: pos.coords.latitude,
-                    lng: pos.coords.longitude,
-                },
-                'My Position'
-            )
-        })
-        .catch((err) => {
-            console.log('err!!!', err)
-        })
+  getPosition()
+    .then((pos) => {
+      console.log('User position is:', pos.coords)
+      document.querySelector(
+        '.user-pos'
+      ).innerText = `Latitude: ${pos.coords.latitude} - Longitude: ${pos.coords.longitude}`
+      mapService.panTo(pos.coords.latitude, pos.coords.longitude)
+      mapService.addMarker(
+        {
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        },
+        'My Position'
+      )
+    })
+    .catch((err) => {
+      console.log('err!!!', err)
+    })
 }
 
 function onPanTo() {
-    console.log('Panning the Map')
-    mapService.panTo(35.6895, 139.6917)
+  console.log('Panning the Map')
+  mapService.panTo(35.6895, 139.6917)
 }
 
 function onChooseLoc(lat, lng) {
-    document.querySelector(
-        '.user-pos'
-    ).innerText = `Latitude: ${lat} - Longitude: ${lng}`
-    mapService.panTo(lat, lng)
+  document.querySelector(
+    '.user-pos'
+  ).innerText = `Latitude: ${lat} - Longitude: ${lng}`
+  mapService.panTo(lat, lng)
 }
 
 function onDeleteLoc(id) {
-    const { lat, lng } = locService.deleteLoc(id)
-    onGetLocs()
+  const { lat, lng } = locService.deleteLoc(id)
+  onGetLocs()
 }
 
 function onCopyLoc(lat, lng) {
-    navigator.clipboard.writeText(
-        `https://github.io/Nirosss/Day34GitCol/index.html?lat=${lat}&lng=${lng}`
-    )
+  navigator.clipboard.writeText(
+    `https://github.io/Nirosss/Day34GitCol/index.html?lat=${lat}&lng=${lng}`
+  )
 }
 
-function onAddressSearch(ev){
-    ev.preventDefault()
-    let address = ev.target.search.value
-    mapService.codeAddress(address)
+function onAddressSearch(ev) {
+  ev.preventDefault()
+  let address = ev.target.search.value
+  mapService.codeAddress(address)
+}
+
+function onSetLocationName(ev) {
+  ev.preventDefault()
+  console.log('WEW GOT',document.querySelector('[name=nameinput]').value)
 }
--- js/services/map.service.js
+++ js/services/map.service.js
@@ -1,10 +1,10 @@
 import { locService } from './loc.service.js'
 
 export const mapService = {
-    initMap,
-    addMarker,
-    panTo,
-    codeAddress
+  initMap,
+  addMarker,
+  panTo,
+  codeAddress,
 }
 
 // Var that is used throughout this Module (not global)
@@ -14,104 +14,104 @@ var marker
 window.initMap = initMap
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
-    return _connectGoogleApi().then(() => {
-        gMap = new google.maps.Map(document.querySelector('#map'), {
-            center: { lat, lng },
-            zoom: 15,
-        }
-        )
-        geocoder = new google.maps.Geocoder()
-        
-        gMap.addListener('click', (ev) => {
-            const lat = ev.latLng.lat()
-            const lng = ev.latLng.lng()
-            const infowindow = new google.maps.InfoWindow()
-            geocodeLatLng(lat,lng, gMap, infowindow) 
-            addMarker()
-        })
-        const locationButton = document.createElement('button')
-        locationButton.classList.add('my-location')
-        locationButton.innerHTML = `<img src="img/my-location.png" title="Go To Your Location"/>`
-        gMap.controls[google.maps.ControlPosition.TOP_CENTER].push(
-            locationButton
-        )
-        locationButton.addEventListener('click', onGetUserPos)
-        
+  return _connectGoogleApi().then(() => {
+    gMap = new google.maps.Map(document.querySelector('#map'), {
+      center: { lat, lng },
+      zoom: 15,
     })
+    geocoder = new google.maps.Geocoder()
 
+    gMap.addListener('click', (ev) => {
+      const lat = ev.latLng.lat()
+      const lng = ev.latLng.lng()
+      const infowindow = new google.maps.InfoWindow()
+      geocodeLatLng(lat, lng, gMap, infowindow)
+      addMarker()
+    })
+    const locationButton = document.createElement('button')
+    locationButton.classList.add('my-location')
+    locationButton.innerHTML = `<img src="img/my-location.png" title="Go To Your Location"/>`
+    gMap.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton)
+    locationButton.addEventListener('click', onGetUserPos)
+  })
 }
 
 function addMarker(loc, title = '') {
-    marker = new google.maps.Marker({
-        position: loc,
-        map: gMap,
-        title: title,
-    })
-    return marker
+  marker = new google.maps.Marker({
+    position: loc,
+    map: gMap,
+    title: title,
+  })
+  return marker
 }
 
 function panTo(lat, lng) {
-    var laLatLng = new google.maps.LatLng(lat, lng)
-    gMap.panTo(laLatLng)
+  var laLatLng = new google.maps.LatLng(lat, lng)
+  gMap.panTo(laLatLng)
 }
 
 function _connectGoogleApi() {
-    if (window.google) return Promise.resolve()
-    const API_KEY = 'AIzaSyDuETDc-5x28cmhJpkzqNwLfi_oKVmzT1E'
-    var elGoogleApi = document.createElement('script')
-    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
-    elGoogleApi.async = true
-    document.body.append(elGoogleApi)
+  if (window.google) return Promise.resolve()
+  const API_KEY = 'AIzaSyDuETDc-5x28cmhJpkzqNwLfi_oKVmzT1E'
+  var elGoogleApi = document.createElement('script')
+  elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
+  elGoogleApi.async = true
+  document.body.append(elGoogleApi)
 
-    return new Promise((resolve, reject) => {
-        elGoogleApi.onload = resolve
-        elGoogleApi.onerror = () => reject('Google script failed to load')
-    })
+  return new Promise((resolve, reject) => {
+    elGoogleApi.onload = resolve
+    elGoogleApi.onerror = () => reject('Google script failed to load')
+  })
 }
 
 function codeAddress(address) {
-    console.log(address)
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == 'OK') {
-        gMap.setCenter(results[0].geometry.location)
-        marker = new google.maps.Marker({
-            map: gMap,
-            position: results[0].geometry.location
-        })
-         var lat= (results[0].geometry.location.lat())
-         var lng= (results[0].geometry.location.lng())
-         locService.addLoc(address, lat, lng)
-        } else {
-        alert('Geocode was not successful for the following reason: ' + status)
-      }
-    })
+  console.log(address)
+  geocoder.geocode({ address: address }, function (results, status) {
+    if (status == 'OK') {
+      gMap.setCenter(results[0].geometry.location)
+      marker = new google.maps.Marker({
+        map: gMap,
+        position: results[0].geometry.location,
+      })
+      var lat = results[0].geometry.location.lat()
+      var lng = results[0].geometry.location.lng()
+      locService.addLoc(address, lat, lng)
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status)
+    }
+  })
+}
+
+function geocodeLatLng(lat, lng, map, infowindow) {
+  let clickedAddress
+  let contentString = `<form onsubmit="onSetLocationName(event)" >
+    <input name="nameinput" type="text" placeholder="Type Location's Name">
+    </form>`
+  const latlng = {
+    lat,
+    lng,
   }
 
-  function geocodeLatLng(lat,lng, map, infowindow) {
-    let clickedAddress
-    const latlng = {
-      lat,
-      lng }
-  
-    geocoder
-      .geocode({ location: latlng })
-      .then((response) => {
-        if (response.results[0]) {
-          map.setZoom(11);
-  
-          const marker = new google.maps.Marker({
-            position: latlng,
-            map: map,
-          });
-  
-          infowindow.setContent(response.results[0].formatted_address);
-          infowindow.open(map, marker);
-          console.log(response.results[0].formatted_address)
-          clickedAddress = (response.results[0].formatted_address)
-        } else {
-          window.alert("No results found");
-        }
-      })
-      .then(() => locService.addLoc(clickedAddress, lat, lng))
-      .catch((e) => window.alert("Geocoder failed due to: " + e));
-  }
+  geocoder
+    .geocode({ location: latlng })
+    .then((response) => {
+      if (response.results[0]) {
+        map.setZoom(11)
+
+        const marker = new google.maps.Marker({
+          position: latlng,
+          map: map,
+        })
+        infowindow.setContent(
+          response.results[0].formatted_address + contentString
+        )
+        infowindow.open(map, marker)
+        console.log(response.results[0].formatted_address)
+        clickedAddress = response.results[0].formatted_address
+      } else {
+        window.alert('No results found')
+      }
+    })
+    .then(() => locService.addLoc(clickedAddress, lat, lng))
+    .catch((e) => window.alert('Geocoder failed due to: ' + e))
+}
